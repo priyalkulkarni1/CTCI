32fc7b51ad1a94b5aede5cacf4eeb9d3ff8f8649
priyalkulkarni1 <priyalkulkarni1@gmail.com>
2019-07-03 00:30:03 -0500

    Update IsUnique.cpp

--- ArraysStrings/IsUnique.cpp
+++ ArraysStrings/IsUnique.cpp
@@ -1,1 +1,17 @@
-#Determine if string has all unique chars, dont use additional data structure
+//Determine if string has all unique chars, dont use additional data structure
+
+
+Bool isUnique( String s)
+{
+  for(int i = 0; i<s.length();i++)
+  {
+    for(int j = i; j<s.length();j++)
+    {
+      if(s[i]==s[j])
+        return false;
+    }
+    
+  }
+  
+  return true;
+}
